102d9c79e4025f679df3944cdf6543ac96290c33
amyoshi <narinarit59@gmail.com>
2025-06-01 01:46:21 +0900

    add draw image

--- GUI_browser/main.cpp
+++ GUI_browser/main.cpp
@@ -3,13 +3,74 @@
 #include "imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
 #include "cpp_parse/html_parser.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "glfw-3.4/stb_img.h"
 
+
+// エラーコールバック
 void glfw_error_callback(int error, const char* description) {
     std::cerr << "GLFW Error " << error << ": " << description << std::endl;
 }
 
-int window(const std::string& text) {
+GLuint LoadTexture(const char* filename) {
+    int width, height, channels;
+    unsigned char* data = stbi_load(filename, &width, &height, &channels, 4);
+    if (!data) {
+        std::cerr << "Failed to load image: " << filename << std::endl;
+        return 0;
+    }
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_image_free(data);
+    return texture;
+}
+
+void RenderImageOverlay(GLuint texture) {
+    ImGui::Begin("Image Overlay");
+
+    // テクスチャをImGuiウィンドウに描画
+    ImVec2 imageSize(300, 300); // 表示サイズ
+    ImGui::Image((ImTextureID)(uintptr_t)texture, imageSize);
+
+
+    ImGui::End();
+}
+
+// グローバル変数
+char url_buffer[256] = "";  // 検索バー入力用バッファ
+std::string display_text = "";  // 表示用テキスト
+
+// 検索バーの描画
+void renderSearchBar(int PORT) {
+    ImGui::Begin("Search Bar");
+
+    // URL入力フィールド
+    ImGui::InputText("Enter file", url_buffer, IM_ARRAYSIZE(url_buffer));
+
+    // Goボタンの処理
+    if (ImGui::Button("Go")) {
+        if (strlen(url_buffer) > 0) {
+            // 入力されたURLをパースして表示テキストを更新
+            display_text = nana::parseHtml(PORT, 0, url_buffer);  // サンプルPORTとプロトコル
+        }
+    }
+
+    ImGui::End();
+}
+
+// メインウィンドウ関数
+int window(int PORT, const std::string& initial_text) {
     // GLFW初期化
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
@@ -17,12 +78,13 @@ int window(const std::string& text) {
         return -1;
     }
 
-    // OpenGLコンテキストの設定
+    // OpenGLコンテキスト設定
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui + GLFW + OpenGL", NULL, NULL);
+    // ウィンドウ作成
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui + GLFW + OpenGL", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -30,7 +92,7 @@ int window(const std::string& text) {
     }
 
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // 垂直同期
+    glfwSwapInterval(1);  // 垂直同期
 
     // ImGui初期化
     IMGUI_CHECKVERSION();
@@ -42,6 +104,15 @@ int window(const std::string& text) {
 
     ImGui::StyleColorsDark();
 
+    // 初期表示テキスト
+    display_text = initial_text;
+    //GLFWwindow* window = glfwCreateWindow(800, 600, "Image Example", NULL, NULL);
+
+    GLuint texture = LoadTexture("test.jpg");
+    if (texture == 0) {
+        glfwTerminate();
+        return -1;
+    }
     // メインループ
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -51,9 +122,14 @@ int window(const std::string& text) {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // ImGuiのウィジェット
-        ImGui::Begin("Hello, world!");
-        ImGui::Text("%s", text.c_str());
+	RenderImageOverlay(texture);
+
+        // 検索バー
+        renderSearchBar(PORT);
+
+        // 表示テキストのウィンドウ
+        ImGui::Begin("HTML Viewer");
+        ImGui::TextWrapped("%s", display_text.c_str());
         ImGui::End();
 
         // 描画
@@ -64,45 +140,38 @@ int window(const std::string& text) {
         glfwSwapBuffers(window);
     }
 
-    // クリーンアップ
+    // 終了処理
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
-
     glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
 }
 
-int main(int argc, char* argv[]){
-	
+int main(int argc, char* argv[]) {
     if (argc < 3) {
-        printf("Usage: %s <port> <protocol (tcp/udp)>\n", argv[0]);
-        exit(1);
+        std::cerr << "Usage: " << argv[0] << " <port> <protocol (tcp/udp)> <filename>" << std::endl;
+        return 1;
     }
 
-    int PORT = atoi(argv[1]);
+    int PORT = std::atoi(argv[1]);
+    int use_udp = (std::strcmp(argv[2], "udp") == 0) ? 1 : 0;
 
-    // プロトコルを指定して処理を分岐
-    int use_udp = 0;
-    if (strcmp(argv[2], "udp") == 0) {
-        use_udp = 1;
-    } else if (strcmp(argv[2], "tcp") != 0) {
-        printf("Invalid protocol. Use 'tcp' or 'udp'.\n");
-        exit(1);
-    }
-    char *filename = argv[3];
+    const char* filename = argv[3];
     std::string parsedHtml = nana::parseHtml(PORT, use_udp, filename);
 
     std::cout << "cui/gui [1:2 mode]: ";
     int mode;
     std::cin >> mode;
 
-    if(mode == 1){
-        printf("%s", parsedHtml.c_str());
+    if (mode == 1) {
+        std::cout << parsedHtml << std::endl;
     } else {
-        window(parsedHtml);
+        window(PORT, parsedHtml);
     }
+
+    return 0;
 }
 
